2767050cc4a9b2884403e8ce2a7174fac3e5a47a
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-07-31 11:02:24 +0800

    update travis for oneapi beta08

--- travis/install-oneapi.sh
+++ travis/install-oneapi.sh
@@ -7,6 +7,5 @@ sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 sudo apt-get update
-sudo apt-get install intel-oneapi-daal-devel intel-oneapi-tbb-devel
+sudo apt-get install intel-oneapi-daal-devel-2021.1-beta08 intel-oneapi-tbb-devel-2021.1-beta08
 
-find /opt
