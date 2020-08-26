5b16e93160885400c2692a9fbe31e51fd4ba02e7
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-08-26 18:35:55 +0800

    update

--- travis/check-pyspark.sh
+++ travis/check-pyspark.sh
@@ -7,4 +7,6 @@ export SPARK_HOME=~/opt/spark-3.0.0-bin-hadoop2.7
 export PATH=$HADOOP_HOME/bin:$SPARK_HOME/bin:$PATH
 
 cd $SPARK_HOME
+
+which python
 ./bin/spark-submit examples/src/main/python/pi.py
--- travis/spark-cluster/setup-cluster.sh
+++ travis/spark-cluster/setup-cluster.sh
@@ -6,8 +6,6 @@ cd $WORK_DIR
 
 echo JAVA_HOME is $JAVA_HOME
 
-sudo ./config-ssh.sh
-
 mkdir ~/opt
 cd ~/opt
 wget https://archive.apache.org/dist/spark/spark-3.0.0/spark-3.0.0-bin-hadoop2.7.tgz
