6d482c7439401637067de91b8e8ffa53e0968c8e
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-08-26 21:49:03 +0800

    update

--- travis/spark-cluster/setup-cluster.sh
+++ travis/spark-cluster/setup-cluster.sh
@@ -21,25 +21,22 @@ cp ./yarn-site.xml ~/opt/hadoop-2.7.7/etc/hadoop/
 cp ./hadoop-env.sh ~/opt/hadoop-2.7.7/etc/hadoop/
 cp ./spark-defaults.conf ~/opt/spark-3.0.0-bin-hadoop2.7/conf
 
-~/opt/hadoop-2.7.7/sbin/stop-yarn.sh
-~/opt/hadoop-2.7.7/sbin/stop-dfs.sh
-
 # create directories
-mkdir -p /var/run/hdfs/namenode
-mkdir -p /var/run/hdfs/datanode
+mkdir -p /tmp/run/hdfs/namenode
+mkdir -p /tmp/run/hdfs/datanode
 
 # hdfs format
 ~/opt/hadoop-2.7.7/bin/hdfs namenode -format
-
-# start hdfs and yarn
-~/opt/hadoop-2.7.7/sbin/start-dfs.sh
-~/opt/hadoop-2.7.7/sbin/start-yarn.sh
 
 export HADOOP_HOME=~/opt/hadoop-2.7.7
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export SPARK_HOME=~/opt/spark-3.0.0-bin-hadoop2.7
 
 export PATH=$HADOOP_HOME/bin:$SPARK_HOME/bin:$PATH
+
+# start hdfs and yarn
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
 
 hadoop fs -ls /
 yarn node -list
