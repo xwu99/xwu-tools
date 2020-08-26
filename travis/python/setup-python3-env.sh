6b695a73402838147a2d6515f6bbb7bc53c3aaed
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-08-26 21:42:49 +0800

    update

--- travis/python/setup-python3-env.sh
+++ travis/python/setup-python3-env.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install python3-pip python3-setuptools python3-wheel
 
-sudo -H pip3 install numpy
+pip3 install --user numpy
 
 echo python is in $(which python) 
 python --version
