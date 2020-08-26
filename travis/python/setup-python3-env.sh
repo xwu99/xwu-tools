6315059302f544e17fe62dce3b7fc99d2f4c66a3
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-08-26 21:04:38 +0800

    update

--- travis/python/setup-python3-env.sh
+++ travis/python/setup-python3-env.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
 apt-get update
-# apt purge python2.7-minimal
-# apt-get install python3 python3-pip python3-setuptools
+apt-get install python3-pip
 
 pip3 install --upgrade pip
 pip3 install numpy
 
-which python
-python --version
-
-which python3
-python3 --version
+echo python is $(which python) $(python --version)
+echo python3 is $(which python3) $(python3 --version)
