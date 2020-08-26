aaec3d6ff10747364bccfc26d76d7adf83945b01
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-08-26 18:16:55 +0800

    add check-pyspark.sh

---
+++ travis/check-pyspark.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export HADOOP_HOME=~/opt/hadoop-2.7.7
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_HOME=~/opt/spark-3.0.0-bin-hadoop2.7
+
+export PATH=$HADOOP_HOME/bin:$SPARK_HOME/bin:$PATH
+
+cd $SPARK_HOME
+./bin/spark-submit examples/src/main/python/pi.py
